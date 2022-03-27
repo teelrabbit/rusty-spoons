ff26d9eb951554dd3c36729acdf43c3be4b7d97b
teelrabbit <xuntitled@protonmail.com>
2022-03-27 15:57:51 -0400

    print statments in rust

---
+++ mod·ule_1/hello.rs
@@ -0,0 +1,14 @@
+//Like other languages the "//" double slash can be used to add comments
+//
+//
+//
+//
+//
+fn main() {
+    //statements inside the main function are called when the code as been
+    //compiled and the binary has been called
+    //bellow is an example of a print statment
+    println!("example print statment!");
+    println!("I'm a Rustacean");
+}
+
